a70400d2ac9c17df4b27be03e88562bd55a84fdf
Ebuka Peter <peterdbrainy5@gmail.com>
2022-10-26 20:04:19 +0100

    First commit

---
+++ src/pages/Landing/Landing.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from '../../components/layout/NavBar/NavBar.component';
+import './Landing.styles.css';
+
+const Landing = () => {
+
+    return (
+        <div>
+            <NavBar/>
+        </div>
+    )
+}
+
+export default Landing;
